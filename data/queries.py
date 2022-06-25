b34e7c2951a5a70f2a29b328a45c609b6ce2c8a2
Alex Andrzejewicz <andrzejewiczalex@gmail.com>
2022-06-25 16:30:33 +0200

    Add pagination nr3

--- data/queries.py
+++ data/queries.py
@@ -4,12 +4,18 @@ from data import data_manager
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
-def get_most_rated_shows(limit=15):
+def get_most_rated_shows(offset=0, limit=15):
     return data_manager.execute_select("""
-    SELECT title, year, runtime, rating,string_agg(genres.name, ', ')As genres ,trailer, homepage
+    SELECT shows.id,title, year, runtime, to_char(rating::float,'999.9')As rating,string_agg(genres.name, ', ')As genres ,trailer, homepage
     FROM shows
     INNER JOIN show_genres ON shows.id = show_genres.show_id
     INNER JOIN genres ON show_genres.genre_id = genres.id
     GROUP BY shows.id
-    ORDER BY rating DESC LIMIT %(limit)s
-    """, {'limit':limit})
+    ORDER BY rating DESC LIMIT %(limit)s offset %(offset)s
+    """, {'limit':limit, 'offset':offset})
+
+def get_shows_page_count(page=15):
+    return data_manager.execute_select("""
+    SELECT count(*)/%(page)s AS page_count
+    FROM shows
+    """, {'page': page})
